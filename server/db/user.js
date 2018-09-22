6b5c6aac9714f2ffd7e02b210aab8e4b4392b8f2
Ekaterina <paekva@yandex.ru>
2018-09-22 20:07:19 +0300

    Adding first db schema for user collection

---
+++ server/db/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+ 
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    login: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    birth_date: {
+        type: Date,
+        validate: {
+            validator: function(date) {
+                return date>=new Date(1900,2,2);
+            },
+            message: 'You\'ve provided wrong bitrh date'
+        }
+    },
+    course: {
+        type: String,
+        required: true
+    },
+    start_date:  {
+        type: Date,
+        validate: {
+            validator: function(date) {
+                return date>this.birth_date;
+            },
+            message: 'You\'ve provided wrong start of the university date'
+        }
+    },
+    end_date: {
+        type: Date,
+        validate: {
+            validator: function(date) {
+                return date>this.start_date;
+            },
+            message: 'You\'ve provided wrong end of the university date'
+        }
+    }
+});
+ 
+var User = mongoose.model('User', userSchema);
+ 
+module.exports = User;
--- server/routes.js
+++ server/routes.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const router = express.Router();
-var mongojs = require('mongojs');
 
-const db_url = 'mongodb://kate:rfnz98grf@ds159782.mlab.com:59782/itmo_info';
-var db = mongojs(db_url, ['users']);
+//Connection options for Data base
+const mongoose = require('mongoose');
+const db_url = 'mongodb://kate:rfnz98grf@ds159782.mlab.com:59782/itmo_info'; 
 
+//Adding our db schema
+const User = require('./db/user');
 
 //Connecting urls to our response
 router.get('/', function (req, res) {  
     res.render('index', { title: 'Hey world', message: 'Hello, it\'s pug there!'});
   });
 
-router.get('/ktu', function (req, res) {
 
-  db.users.findOne(function(err, user) {
-    if( err) res.send(err);
-    else if(!user) req.send('no user was found');
-    else res.send(user);
+router.get('/user/save', function (req, res) {
+
+  mongoose.connect(db_url, function (err) {
+ 
+    if (err) throw err;
+    console.log('Successfully connected');
+  
+    let sasha = new User(
+      {
+        _id: new mongoose.Types.ObjectId(),
+        login: "sasha",
+        password: "1234",
+        birth_date: new Date(1998,6,4),
+        course: "Информатика и вычислительная техника",
+        start_date: new Date(2016,8,2),
+        end_date: new Date(2020,7,32),
+      }
+    );
+
+    sasha.save(function(err) {
+      if (err) throw err;
+       
+      console.log('User successfully saved.');
+    }
+    );
+
   });
 
   });
